666e0d7c1792c2d899fdc383952977776cc32168
Sahibul Nuzul Firdaus <sahibulnuzulfirdaus13@gmail.com>
2021-05-31 11:33:02 +0700

    ✨ NEW FEATURE: Validate Token - JWT

--- auth/service.go
+++ auth/service.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"moyu/consts"
 
 	"github.com/dgrijalva/jwt-go"
@@ -8,6 +9,7 @@ import (
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -23,11 +25,31 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
 
-	signedToken, err := token.SignedString(consts.Const())
+	signedToken, err := token.SignedString(consts.SecretKey())
 
 	if err != nil {
 		return signedToken, err
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+
+	token, err := jwt.Parse(encodedToken, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(consts.SecretKey()), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
+
 }
